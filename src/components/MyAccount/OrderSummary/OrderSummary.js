7234c7c60b8fbebc0c75d746b39db7b6a71038bf
Taras Yurchyshyn <yts.prof@gmail.com>
2025-02-12 00:42:09 +0200

    minor fix

--- src/components/MyAccount/OrderSummary/OrderSummary.js
+++ src/components/MyAccount/OrderSummary/OrderSummary.js
@@ -62,49 +62,48 @@ const OrdersSummary = () => {
         </table>
       </div>
 
-    {/* Mobile */}
-<div className="md:hidden overflow-x-auto">
-  <div className={`${isSingleOrder ? "w-full" : "w-max"}`}>
-    <table className="border-collapse  w-full">
-      <tbody>
-        {headers.map((header, rowIndex) => (
-          <tr key={rowIndex} className=" border border-dark/70 text-[15px]">
-          <td className="relative min-w-[185px] px-2 py-2 font-bold sticky left-0 bg-[#F4F4F4] z-10
+      {/* Mobile */}
+      <div className="md:hidden overflow-x-auto">
+        <div className={`${isSingleOrder ? "w-full" : "w-max"}`}>
+          <table className="border-collapse  w-full">
+            <tbody>
+              {headers.map((header, rowIndex) => (
+                <tr key={rowIndex} className=" border border-dark/70 text-[15px]">
+                  <td className=" min-w-[185px] px-2 py-2 font-bold sticky left-0 bg-[#F4F4F4] z-10
   before:absolute before:top-0 before:left-0 before:w-[1px] before:h-full before:bg-dark/70
   after:absolute after:top-0 after:right-0 after:w-[1px] after:h-full after:bg-dark/70">
-  {header}
-</td>
+                    {header}
+                  </td>
 
 
 
 
-            {orders.map((order, colIndex) => (
-              <td
-                key={colIndex}
-                className={`  px-2 py-1 border border-dark/70 ${
-                  isSingleOrder ? "w-full" : "max-w-[160px]"
-                }`}
-              >
-                <div className="min-h-[55px] flex items-center">
+                  {orders.map((order, colIndex) => (
+                    <td
+                      key={colIndex}
+                      className={`  px-2 py-1 border border-dark/70 ${isSingleOrder ? "w-full" : "max-w-[160px]"
+                        }`}
+                    >
+                      <div className="min-h-[55px] flex items-center">
 
-            
-                {[
-                  order.orderNumber,
-                  order.orderDate,
-                  order.author,
-                  order.bookName,
-                  order.totalPaid,
-                  order.status,
-                ][rowIndex]}
-                    </div>
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-</div>
+
+                        {[
+                          order.orderNumber,
+                          order.orderDate,
+                          order.author,
+                          order.bookName,
+                          order.totalPaid,
+                          order.status,
+                        ][rowIndex]}
+                      </div>
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
 
     </div>
   );
