4b4c7eb7a20cab32accbd44af419298a25aeca7c
Taras Yurchyshyn <yts.prof@gmail.com>
2025-03-01 21:51:39 +0200

    my account changes

--- src/components/Hero/HeroMyAccount.js
+++ src/components/Hero/HeroMyAccount.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const heroData = {
   title: "My Account",
-  subtitle: "Welcome to your Book Tailor account page!",
+  subtitle: "Welcome to Your account page",
   description:
-    "Easily track your orders, check their status, and stay updated every step of the way. Whether you're following the progress of your book or reviewing past purchases, everything you need is right here.",
+    "Track your orders and stay updated on their status with ease. From viewing order details to tracking shipments, this is your one-stop hub for managing all your Book Tailor purchases. ",
   image: "/images/my-account/my-account-hero.png", 
 };
 
 const HeroMyAccount = () => {
   return (
-    <div className="md:bg-[#F9F6EB] p-3 md:p-12 md:max-h-[343px] text-dark font-inter">
+    <div className="md:bg-[#FFFBEE] p-3 md:p-12 md:max-h-[343px] text-dark font-inter">
       <div className="flex flex-col md:flex-row items-center md:items-start md:gap-[58px] max-w-[833px] mx-auto">
         <h1 className="text-[36px]  md:hidden font-bold ">{heroData.title}</h1>
-        <div className="w-[251px] h-[251px] md:w-[251px] d:h-[251px] mt-4 md:mt-0">
+        <div className="w-[193px] h-[193px]  mt-4 md:mt-0">
         <img
           src={heroData.image}
           alt="Book with tea"
@@ -23,7 +23,7 @@ const HeroMyAccount = () => {
 
         <div className="flex-1  ">
           <h1 className="hidden md:block text-[46px] leading-none font-semibold ">{heroData.title}</h1>
-          <h2 className="text-[21px] md:text-[18px] font-semibold md:mt-8 mt-2">
+          <h2 className="text-[18px] font-semibold md:mt-8 mt-2">
             {heroData.subtitle}
           </h2>
           <p className="text-[18px] md:mt-2">{heroData.description}</p>
--- src/components/MyAccount/OrderDetails/OrderDetails.js
+++ src/components/MyAccount/OrderDetails/OrderDetails.js
@@ -39,96 +39,93 @@ const stepsName = [
 
 const OrderDetails = () => {
   return (
-    <div className="w-full bg-[#F9F6EB] shadow-detailsShadow p-4 md:pt-4 md:pr-3 md:pb-[68px] ">
-      <h1 className="text-xl md:text-[23px] font-bold text-center mb-6">
-        Details for Order #{orderDetails.orderId}
+    <div className="w-full bg-[#FFFBEE] shadow-detailsShadow pt-5 pb-10 px-4 md:px-10  md:pb-4 border-[0.5px] border-[#A6A6A6] rounded-[3px]">
+      <h1 className="text-[18px] font-bold text-center ">
+        Order Number: <span className="font-normal">{orderDetails.orderId}</span>
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:px-24 text-dark">
+      <div className="mt-4 flex flex-col md:flex-row  justify-between  items-center text-dark">
         {/* Ordered Item */}
         <div className="flex-1 ">
-          <h2 className="font-semibold text-base md:text-[18px] mb-2">Ordered Item</h2>
           <div className="flex items-center gap-2 text-base leading-[22px]">
             <img
               src={orderDetails.orderedItem.image}
               alt="Book Cover"
-              className="w-16 h-auto md:w-24 max-w-[72px]"
+              className="w-14 h-20 md:w-14 md:h-20 max-w-[72px]"
             />
             <p className="min-w-[25px]">x {orderDetails.orderedItem.quantity}</p>
             <div className="w-fit text-[14px] md:text-base">
-              <p className="font-semibold">{orderDetails.orderedItem.author}</p>
-              <p className="italic leading-[18px] font-medium">{orderDetails.orderedItem.title}</p>
+              <p className="font-medium">{orderDetails.orderedItem.author}</p>
+              <p className="leading-[18px] ">{orderDetails.orderedItem.title}</p>
 
             </div>
           </div>
         </div>
+        <div className="text-left  md:max-w-[238px]">
+          <p className="mt-4 md:mt-2 mb-2 leading-[14px] text-[15px] md:text-base text-dark  md:leading-[18px]">
+            Place a new order if you’d like more copies of your book!
+          </p>
+          <a className="mt-2 md:mt-6 w-fit md:f-full px-4 md:px-0 max-w-[330px] md:w-[238px] h-[50px] md:mx-auto flex md:justify-center items-center bg-orange text-white font-semibold text-[23px] rounded-[3px] gap-1 shadow-heroBtnShadow group" href="/create-book"><span className="mb-[2px]">Reorder</span>
+            <span className="group-hover:translate-x-1.5 transition"><svg className="" width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_0_615)"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.82983 0.670075C2.29032 0.209585 3.03692 0.209585 3.49741 0.670073L9.61156 6.78422C9.83269 7.00535 9.95692 7.30528 9.95692 7.618C9.95692 7.93074 9.83269 8.23067 9.61156 8.4518L3.49741 14.5659C3.03692 15.0265 2.29032 15.0265 1.82983 14.5659C1.36934 14.1054 1.36934 13.3589 1.82983 12.8984L7.11019 7.618L1.82983 2.33765C1.36934 1.87716 1.36934 1.13056 1.82983 0.670075Z" fill="white"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M8.9938 0.670075C9.45429 0.209585 10.2009 0.209585 10.6614 0.670073L16.7755 6.78422C16.9966 7.00535 17.1208 7.30528 17.1208 7.618C17.1208 7.93074 16.9966 8.23067 16.7755 8.4518L10.6614 14.5659C10.2009 15.0265 9.45429 15.0265 8.9938 14.5659C8.53332 14.1054 8.53332 13.3589 8.9938 12.8984L14.2742 7.618L8.9938 2.33765C8.53332 1.87716 8.53332 1.13056 8.9938 0.670075Z" fill="white"></path></g><defs><clipPath id="clip0_0_615"><rect width="17.0323" height="15.7221" fill="white" transform="translate(0.648438 0.193359)"></rect></clipPath></defs></svg></span></a>
 
-        {/* Payment Summary */}
-        <div className="flex-1 md:mt-0 md:ml-28">
-          <h2 className="font-semibold text-base md:text-[18px] mb-4">Payment Summary</h2>
-          <div className="">
-            {Object.entries(orderDetails.paymentSummary).map(([key, value]) => (
-              <div key={key} className="flex justify-between text-dark/70 font-medium md:text-[13px] text-[15px]">
-                <span className="capitalize">{key.replace(/([A-Z])/g, " $1")}</span>
-                <span>{value}</span>
-              </div>
-            ))}
-          </div>
-          <button className="mt-2 py-0 px-2 w-auto border-2 border-dark/50 font-medium rounded-md text-dark/70 text-[13px] ">
-            View My Receipt
-          </button>
+
         </div>
+
       </div>
 
-      <hr className="my-6 md:mx-32 border-0 h-[0.5px] bg-dark/50" />
+      <hr className="my-6 border-0 h-[0.5px] bg-[#AFAFAF]" />
 
-      <div className=" w-full leading-[17px] md:mt-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
+      <div className=" w-full leading-[17px] ">
+        <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-6">
 
-          <div className="md:pl-24 leading-[27px]">
-            <h2 className="font-semibold text-base md:text-[18px] text-dark ">Order date</h2>
-            <p className="leading-[14px] mt-0 text-[14px] md:text-base text-dark font-medium">Jan 5, 2025</p>
-            <h2 className="mt-4 md:mt-10 font-semibold text-base md:text-[18px] text-dark  ">Status</h2>
-            <p className="text-[14px] leading-[14px] md:text-base text-dark font-medium">Processing</p>
+          <div className="md-gap-[11px] flex flex-col order-2">
 
+            <h2 className="font-semibold text-[18px] leading-[27px] text-dark  flex items-center">
+              Details
+            </h2>
 
-            <div className="text-left mt-6 md:mt-12 md:max-w-[228px]">
-              <h2 className="md:mt-9 mt-6 font-semibold text-base md:text-[18px] text-dark">Need More Copies?</h2>
-              <p className="md:mt-2 mb-2 leading-[14px] text-[14px] md:text-base text-dark font-medium md:leading-[18px]">
-                Place a new order if you’d like more copies of your book!
-              </p>
-              <button className="mx-auto flex gap-1 items-center group py-0 px-2 border border-black w-auto bg-orange text-white font-semibold rounded">
-                Reorder <span className="group-hover:translate-x-1 transition">&raquo;</span>
-              </button>
-
-            </div>
-          </div>
-          <hr className="md:hidden my-1 border-0 h-[0.5px] bg-dark/50" />
-          <div>
-            <div className="md:pr-8 md-gap-[11px] flex flex-col md:ml-28">
-              <div className="flex justify-between">
-                <h2 className="font-semibold text-base md:text-[18px] leading-[27px] text-dark  flex items-center">
-                  Shipment Information
-                </h2>
-
-              </div>
-              <div className="text-[13px] font-semibold text-dark">
-                <p className="">Recipient Name</p>
+            <div className="flex flex-col mt-2 md:mt-5 gap-3">
+              <div className=" text-dark flex gap-2">
+                <p className=" font-medium">Order date:</p>
                 <p className="text-dark/70">Ashley Brook</p>
               </div>
-              <div className="mt-4 md-[mt-0] text-[13px] font-semibold text-dark">
-                <p className="">Address</p>
+              <div className="text-dark flex gap-2">
+                <p className="font-medium">Recipient Name:</p>
+                <p className="text-dark/70">Ashley Brook</p>
+              </div>
+              <div className="text-dark flex gap-2">
+                <p className="font-medium">Address:</p>
                 <p className="text-dark/70">123 Main Street, New York, NY, 10001</p>
               </div>
-              <div className="mt-4  text-[13px] font-semibold text-dark">
-                <p className="text-dark">Shipping Method</p>
+              <div className="text-dark flex gap-2">
+                <p className="font-medium">Shipping Method:</p>
                 <p className="text-dark/70">Standard</p>
               </div>
-
-              <h2 className="text-center md:text-left font-semibold text-base md:text-[18px] text-dark mt-10 md:mt-[18px] ">Delivery Status update</h2>
-
             </div>
-            <div className="md:-mt-6">
+
+
+          </div>
+
+          {/* Payment Summary */}
+          <div className="flex-[0.8] md:mt-0 order-1 md:order-2">
+            <h2 className="font-semibold text-[18px] leading-[27px]">Payment Summary</h2>
+            <div className="flex flex-col mt-2 md:mt-5 gap-3">
+              {Object.entries(orderDetails.paymentSummary).map(([key, value]) => (
+                <div key={key} className="flex justify-between ">
+                  <p className="capitalize font-medium">{key.replace(/([A-Z])/g, " $1")}</p>
+                  <p className="text-dark/70">{value}</p>
+                </div>
+              ))}
+            </div>
+            <button className="mt-4 px-2 py-1 w-auto border-[0.8px] border-dark/50  rounded-[3px] text-dark/70 text-[13px] ">
+              View My Receipt
+            </button>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex flex-col items-center max-w-[390px] mx-auto">
+            <h2 className="text-center md:text-left font-semibold text-[18px] leading-[27px] text-dark mt-10 md:mt-[18px] ">Status </h2>
+            <div className="w-full -mt-6">
               <ProgressBar
                 progressStep={1.5}
                 currentStep={1}
@@ -138,6 +135,7 @@ const OrderDetails = () => {
                 myAccount={true}
               />
             </div>
+
           </div>
         </div>
       </div>
