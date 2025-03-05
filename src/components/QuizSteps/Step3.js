cd36afd2713660cfaf7524922235de800ad03b12
Taras Yurchyshyn <yts.prof@gmail.com>
2025-03-05 09:44:25 +0200

    Score system updated

--- src/components/QuizSteps/Step3.js
+++ src/components/QuizSteps/Step3.js
@@ -48,107 +48,6 @@ function Step3({ setIsButtonDisabled, setProgressStep, textError }) {
   const [score, setScore] = useState(0);
   const [answeredQuestions, setAnsweredQuestions] = useState(0);
 
-
-  // const questions = [
-  //   {
-  //     value: "1",
-  //     label: `What does ${author} do?`,
-  //     example: "She works as a machine learning engineer at DataCore AI, a company focused on predictive analytics tools for the healthcare industry. Her job revolves around designing algorithms that help doctors make better decisions using patient data.",
-  //     tip: "Include the role, company name, industry focus, and a specific project or achievement",
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "2",
-  //     label: `Where does ${author} live?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "3",
-  //     label: `What are ${author}’s hobbies?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "4",
-  //     label: `Who is ${author}’s family?`,
-  //     example: "Mom: Karen Dawson (female, 52) is a history teacher at the local high school. She spends her weekends gardening, especially taking care of her orchids, and watching NCIS. She loves trying to figure out the cases before the characters do.",
-  //     tip: "Specify the relationship, name, age, and gender. Add details like job, interests, or hobbies for a personal touch.",
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "5",
-  //     label: `What are ${author}’s backstories?`,
-  //     example: false,
-  //     tip: "Share stories about childhood, major life events, memorable moments, or challenges.",
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "6",
-  //     label: `What’s one thing ${author} really good at?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "7",
-  //     label: `What’s a topic ${author} can talk about for hours without getting bored?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "8",
-  //     label: `What’s something ${author} hilariously bad at?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "9",
-  //     label: `Does ${author} have any funny habits or routines?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "10",
-  //     label: `What’s ${author}’s biggest dream or goal?`,
-  //     example: false,
-  //     tip: false,
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "11",
-  //     label: `What are ${author}’s favorite places?`,
-  //     example: false,
-  //     tip: "Describe what the place is like and how you spend your time there.",
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "12",
-  //     label: `Who are ${author}’s best friends?`,
-  //     example: "Sarah Thompson (30, female) is a graphic designer at a marketing agency. She enjoys hiking and photography. They have been friends since college and often catch up over coffee.",
-  //     tip: "Include each friend's name, age, and gender. Add details like job, interests, or hobbies for a personal touch.",
-  //     isDisabled: false,
-  //   },
-  //   {
-  //     value: "13",
-  //     label: `Who are ${author}’s colleagues?`,
-  //     example: false,
-  //     tip: "Include their name, gender, and role.",
-  //     isDisabled: false,
-  //   },
-  // ];
-
-  // const handleQuestionSelectChange = (option) => {
-  //   if (!questionsAndAnswers.some((qa) => qa.question === option.label)) {
-  //     addQuestionAndAnswer(option.label, "");
-  //   }
-  // };
-
   const handleQuestionSelectChange = (option) => {
     if (!questionsAndAnswers.some((qa) => qa.value === option.value)) {
       addQuestionAndAnswer(option.value, ""); 
@@ -172,19 +71,19 @@ function Step3({ setIsButtonDisabled, setProgressStep, textError }) {
 
 
   const weightConfig = {
-    "1": { levels: [{ words: 20, score: 4 }, { words: 10, score: 3 }, { words: 1, score: 2 }] },
+    "1": { levels: [{ words: 16, score: 4 }, { words: 10, score: 3 }, { words: 4, score: 2}, { words: 1, score: 1 }] },
     "2": { levels: [{ words: 1, score: 1 }] },
-    "3": { levels: [{ words: 10, score: 4 }, { words: 5, score: 3 }, { words: 1, score: 2 }] },
-    "4": { levels: [{ words: 40, score: 3 }, { words: 20, score: 2 }, { words: 10, score: 1 }] },
-    "5": { levels: [{ words: 30, score: 2 }, { words: 15, score: 2 }, { words: 5, score: 1 }] },
+    "3": { levels: [{ words: 15, score: 4 }, { words: 9, score: 3 }, { words: 4, score: 2 }, { words: 1, score: 1 }] },
+    "4": { levels: [{ words: 30, score: 3 }, { words: 10, score: 2 }, { words: 1, score: 1 }] },
+    "5": { levels: [{ words: 20, score: 3 }, { words: 9, score: 2 }, { words: 1, score: 1 }] },
     "6": { levels: [{ words: 5, score: 2 }, { words: 1, score: 1 }] },
-    "7": { levels: [{ words: 10, score: 4 }, { words: 5, score: 3 }, { words: 1, score: 2 }] },
-    "8": { levels: [{ words: 5, score: 2 }, { words: 1, score: 1 }] },
+    "7": { levels: [{ words: 11, score: 4 }, { words: 7, score: 3 }, { words: 3, score: 2 }, { words: 1, score: 1 }] },
+    "8": { levels: [{ words: 1, score: 1 }] },
     "9": { levels: [{ words: 1, score: 1 }] },
     "10": { levels: [{ words: 1, score: 1 }] },
     "11": { levels: [{ words: 5, score: 2 }, { words: 1, score: 1 }] },
-    "12": { levels: [{ words: 20, score: 3 }, { words: 10, score: 2 }, { words: 5, score: 1 }] },
-    "13": { levels: [{ words: 20, score: 3 }, { words: 10, score: 2 }, { words: 5, score: 1 }] }
+    "12": { levels: [{ words: 16, score: 3 }, { words: 8, score: 2 }, { words: 1, score: 1 }] },
+    "13": { levels: [{ words: 16, score: 3 }, { words: 8, score: 2 }, { words: 1, score: 1 }] }
   };
 
 
@@ -224,8 +123,6 @@ function Step3({ setIsButtonDisabled, setProgressStep, textError }) {
   };
 
 
-
-
   const countWords = (text) => {
     const trimmedText = text.trim();
     if (trimmedText.length === 0) return 0;
@@ -235,10 +132,8 @@ function Step3({ setIsButtonDisabled, setProgressStep, textError }) {
   };
 
 
-
-
   let qualityLevel = "Empty bar";
-  if (score >= 13 && answeredQuestions >= 6) qualityLevel = "Excellent";
+  if ((score >= 13 && answeredQuestions >= 6) || (score >= 8 && answeredQuestions >= 8)) qualityLevel = "Excellent";
   else if (score >= 9 && answeredQuestions >= 5) qualityLevel = "Good";
   else if (score >= 5 && answeredQuestions >= 3) qualityLevel = "OK";
   else if (score >= 1 && answeredQuestions >= 1) qualityLevel = "Basic";
